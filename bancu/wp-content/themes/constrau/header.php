6977d1bb08701a7d5b9414fac475fe5e29e8de4a
hypit-dev-lanhk <113501960+hypit-dev-lanhk@users.noreply.github.com>
2023-11-07 14:52:42 +0700

    1

--- bancu/wp-content/themes/constrau/header.php
+++ bancu/wp-content/themes/constrau/header.php
@@ -7,9 +7,7 @@
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <meta name="robots" content="noindex">
 	<link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700&display=swap" rel="stylesheet">
-	<meta name="googlebot" content="noindex">
     <?php wp_head(); ?>
 </head>
 

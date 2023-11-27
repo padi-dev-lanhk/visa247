fad01bb6d1d2076710cdb02066edfb071734f07e
hypit-dev-lanhk <113501960+hypit-dev-lanhk@users.noreply.github.com>
2023-11-27 16:19:59 +0700

    1

--- bancu/wp-config.php
+++ bancu/wp-config.php
@@ -1,4 +1,7 @@
 <?php
+/** Enable W3 Total Cache */
+define('WP_CACHE', true); // Added by W3 Total Cache
+
 /**
  * The base configuration for WordPress
  *
@@ -23,13 +26,13 @@
 define( 'DB_NAME', 'visa247' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'visa247_dev' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Visa247D!@YayqHXat@#$AQm4Cry22LM' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '178.128.95.94' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
@@ -48,14 +51,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'hx}olbie4UJG[Qj$kBpJBJ8GTG ZBT-mJ@pxDy3n;LFH&-*X!y@[nJ}gDrv5!Tal' );
-define( 'SECURE_AUTH_KEY',  '%&uS*^ wEIi32BZV[xV;07DYN.@u8jmX2!*I9N)f,0R~D,?Owddq`? #gb;1@oQF' );
-define( 'LOGGED_IN_KEY',    '*4PK4b%%h^aK>|c?^>ybw:.D%UB:<9ZRM6qXZ##.LW>L@=QkL-NXG.n;7+(H@%Bd' );
-define( 'NONCE_KEY',        'bESX.E/F(Z8]|nK---&UOW`g!:Dv=BggM>>4cS}zo %ulu@#KaC:{+/CAj81-j.j' );
-define( 'AUTH_SALT',        'j^/}rG;nQc*5rzS0Iu9IYUe}yZU1+x<zhr4y8&/pcVhmw)$v7} e9vQjH%no])Zg' );
-define( 'SECURE_AUTH_SALT', 'YEVdKh/=>`#=H]0RMn2deui5^$~4:9r2y|}rK2D_nTy<h:<rHw%,vT+,yYINym@#' );
-define( 'LOGGED_IN_SALT',   '`!;e.v:iv*7$+hX@zRdn0mCL5`CjvqK1)}:HM<Rer#F6f)Z.0/kx_VwOQ&[#lL|8' );
-define( 'NONCE_SALT',       'fT{nYwcvx8;d{gHxV^taFw}$bqEJzqGJJfsV|(T5%,sV=3YS=Xm}j2I.;Ne&Xf)Z' );
+define( 'AUTH_KEY',         'hAsTra.||H~q^TA6nP7G?M3#yC0L?)~x^R7&1JY6Eigw c-qa}YUE&yn:}u9l$Jd' );
+define( 'SECURE_AUTH_KEY',  'aMHu-8IN eDkfpw5)(@J+= PQlw6Ji(t!ro]eChfXR2;N0Tp%``*EkAO0*MEt%+Q' );
+define( 'LOGGED_IN_KEY',    'WH=v8ZQbWFey T=3C{|QCz uks{gCj0MZ6MJ8vCP#.i$i&)9~t}E8%aB=c-JW0]7' );
+define( 'NONCE_KEY',        'P]`h3B@`W(Dv`)jDX3X>)&V8!$C0L`A]@r?kUK=zGg2ch0)*VCpqzB-+LU9yWA:a' );
+define( 'AUTH_SALT',        'Q/:pkW90?NWLaiB)=(Um`Y<b=u96%UJDXLLSPm!|@ ylDT{5?^H@;(C!U|+rc)gR' );
+define( 'SECURE_AUTH_SALT', 'fI:@PV3Udd0MX=]w-k4dgPCN1.TfQUQ/8z1KALJ4>Tls-ztF=78W+v:u&f 5xf@9' );
+define( 'LOGGED_IN_SALT',   '{=1/RW`Xhsun02ISrQiL,H:e`=b5Uflr6!UbX,}b{O&QIa-9:fAnI)qa4|)~x(bM' );
+define( 'NONCE_SALT',       'Ub+cl-k21:hJuJRM+5b/x S{QG>3R~!4]GIFlFrotx(GwV!oq`(Oo#(v~ %,eWD!' );
 
 /**#@-*/
 
@@ -79,7 +82,18 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
+
+
+define( 'WP_DEBUG_LOG', true );
+
+
+define( 'WP_DEBUG_DISPLAY', false );
+
+
+@ini_set( 'display_errors', 0 );
+
+define( 'WP_AUTO_UPDATE_CORE', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
